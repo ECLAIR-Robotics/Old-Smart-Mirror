452c770f72e232040187a526aa20b7b25cb79abd
Arpan Dhatt <ary.dhatt@gmail.com>
2021-10-16 12:29:56 -0500

    trying to get https json requests working

--- jarvis.cpp
+++ jarvis.cpp
@@ -1,40 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <GL/glew.h>
-#include <GL/glut.h>
-#include <GLFW/glfw3.h>
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "httplib.h"
+#include <iostream>
+#include <nlohmann/json.hpp>
 
-#include <glm/glm.hpp>
+using namespace nlohmann;
 
-//#include "constants.hpp"
-
-using namespace glm;
-
-void drawTriangle()
-{
-    glClearColor(0.4, 0.4, 0.4, 0.4);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glColor3f(1.0, 1.0, 1.0);
-    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-
-        glBegin(GL_TRIANGLES);
-                glVertex3f(-0.7, 0.7, 0);
-                glVertex3f(0.7, 0.7, 0);
-                glVertex3f(0, -1, 0);
-        glEnd();
-
-    glFlush();
-}
-
-int main(int argc, char **argv)
-{
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("OpenGL - Creating a triangle");
-    glutDisplayFunc(drawTriangle);
-    glutMainLoop();
-    return 0;
+int main() {
+    httplib::Client cli("https://dog.ceo");
+    auto res = cli.Get("/api/breeds/image/random");
+    std::cout << res->body << std::endl;
+    std::cout << res->status;
 }
